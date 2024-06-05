782b368ad026db65bc85e27cbe093e4d5d618d28
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-06-06 00:23:17 +0200

    commit

--- Code/CreateOrderForm.cs
+++ Code/CreateOrderForm.cs
@@ -17,10 +17,43 @@ namespace Projektni_zadatak
             InitializeComponent();
         }
 
+        String customerID = LoginForm.customerID;
+        MySqlDataReader reader;
+        public static String lastOrder;
+        int state;
+        int postojiArtikal = 0;
+        int artikalValidan = 0;
+        int kolicinaStanjeKolicina;
+        int imaNema = 0;
 
+        private void CheckPoslana(String lastOrder, String customerID) {
+            String queryState = "SELECT STATE FROM narudzbenica where narudzbenica_ID = '" + lastOrder + "' AND kupac_id = '" + customerID + "' ORDER BY state DESC LIMIT 1 ";
+            Utility.executeQuery(queryState, 2);
+            reader = Utility.reader;
+            reader.Read();
+            if (reader.HasRows)
+            {
+                state = Convert.ToInt32(reader[0]);
+            }
+            else
+            {
+                Utility.kreairajNarudžbenicu(customerID);
+                state = 0;
+            }
+            Utility.stopQuery(2);
+        }
 
+        private void ProvjeriStanje() {
+            String queryTwo = "SELECT kolicina_stanje FROM skladiste WHERE artikal_id='" + textBoxID.Text + "'";
+            Utility.executeQuery(queryTwo, 2);
+            reader = Utility.reader;
+            reader.Read();
 
-        private int CheckTextBoxId() {
+            kolicinaStanjeKolicina = Convert.ToInt32(reader[0]) - Convert.ToInt32(textBoxKOL.Text);
+        }
+
+        private int CheckTextBoxId()
+        {
             if (textBoxID.Text == "")
             {
                 MessageBox.Show("Molimo vas unesite pravilan id artikla.");
@@ -30,16 +63,20 @@ namespace Projektni_zadatak
             else return 0;
         }
 
-        String customerID = LoginForm.customerID;
-        MySqlDataReader reader;
-        public static String lastOrder;
-        DataTable tabela;
-        int state;
-        int postojiArtikal = 0;
-        int dobar = 0;
-
-
-        int pritisnuto = 0;
+        private void CheckValidId() {
+            string queryCheckArtikal = "SELECT artikal_id FROM artikal";
+            Utility.executeQuery(queryCheckArtikal, 2);
+            reader = Utility.reader;
+            while (reader.Read())
+            {
+                if (reader[0].ToString() == textBoxID.Text)
+                {
+                    postojiArtikal = 1;
+                    break;
+                }
+                else postojiArtikal = 0;
+            }
+        }
 
         private void ModificirajGridView(DataGridView dgv)
         {
@@ -78,14 +115,25 @@ namespace Projektni_zadatak
         private void CreateOrderButton_Click(object sender, EventArgs e)
         {
             TakeLastOrder();
+            //
+            String queryHelper = "SELECT artikal_id FROM narudzbenica n, stavka_narudzbenice sn WHERE n.narudzbenica_id = sn.narudzbenica_id AND n.kupac_id = '" + customerID + "' AND n.narudzbenica_id = '" + lastOrder + "'";
+            Utility.executeQuery(queryHelper, 2);
+            reader = Utility.reader;
+            if (reader.HasRows)
+            {
+            }
+            else {
+                MessageBox.Show("Ne postoje artikli u korpi koje bi mogli naručiti.");
+                return;
+            }
+
+            //
             String queryA = "UPDATE narudzbenica SET kupac_id = '" + customerID + "', datum_narudzbe = '" + DateTime.Now.ToString("yyyy-MM-dd") + "', state = '1' WHERE kupac_id = '"+customerID+"'";
             Utility.executeQuery(queryA, 0);
             MessageBox.Show("Narudžba je kreirana.");
             Utility.pritisnuto = 0;
 
-            String query = "INSERT INTO narudzbenica(kupac_id,datum_narudzbe, state) VALUES('" + customerID + "','" + DateTime.Now.ToString("yyyy-MM-dd") + "', 0)";
-            Utility.executeQuery(query, 0);
-            Utility.stopQuery(0);
+            Utility.kreairajNarudžbenicu(customerID);
 
             textBoxTotal.Text = "";
             TakeLastOrder();
@@ -120,18 +168,12 @@ namespace Projektni_zadatak
 
         private void Add_Click(object sender, EventArgs e)
         {
+            errorProvider1.Clear();
             try
             {
-                string queryCheckArtikal = "SELECT artikal_id FROM artikal";
-                Utility.executeQuery(queryCheckArtikal, 2);
-                reader = Utility.reader;
-                while (reader.Read()) {
-                    if (reader[0].ToString() == textBoxID.Text)
-                    {
-                        postojiArtikal = 1;
-                        break;
-                    };
-                }
+                //Provjerava da li postoji id sa ovim artiklom
+                CheckValidId();
+                //Ako je postoji artikal = 0, znači da ne postoji id sa ovim artiklom
                 if (postojiArtikal == 0) {
                     MessageBox.Show("Molimo vas unesite pravilan id artikla.");
                     errorProvider1.SetError(textBoxID, "Molimo vas unesite pravilan id artikla.");
@@ -139,74 +181,51 @@ namespace Projektni_zadatak
                 }
                 Utility.stopQuery(2);
 
-                string queryProvjeriObrisi = "SELECT artikal_id FROM stavka_narudzbenice sn, narudzbenica n WHERE sn.artikal_id = 1  AND sn.narudzbenica_id = n.narudzbenica_id AND n.kupac_id = 2";
-
-
-                if (lastOrder == null) {
-                    String query = "INSERT INTO narudzbenica(kupac_id,datum_narudzbe, state) VALUES('" + customerID + "','" + DateTime.Now.ToString("yyyy-MM-dd") + "', 0)";
-                    Utility.executeQuery(query, 0);
-                    Utility.stopQuery(0);
-                }
-
+                //Ako ne postoji prva narudžbenica (ako je tek aplikacija pokrenuta ili ako se izbrišu narudžbe), kreiraj novu narudžbenicu
+                if (lastOrder == null) Utility.kreairajNarudžbenicu(customerID);
+                
+                //Kada se kreira nova narudzba, "uzmi je"
                 TakeLastOrder();
-                String queryState = "SELECT STATE FROM narudzbenica where narudzbenica_ID = '"+lastOrder+"' AND kupac_id = '"+customerID+"' ORDER BY state DESC LIMIT 1 ";
-                Utility.executeQuery(queryState, 2);
-                reader = Utility.reader;               
-                reader.Read();
-                if (reader.HasRows)
-                {
-                    state = Convert.ToInt32(reader[0]);
+
+                //Provjera da li je narudzbenica poslana
+                CheckPoslana(lastOrder, customerID);
+
+                if (state == 1) Utility.pritisnuto = 0;
+                if (state == 0)  Utility.pritisnuto = 1;
+         
+                //Ako dugme dodaj nije bilo pritisnuto prvi put, ili je pritisnuto, a nema narudzbenice, kreiraj narudzbenicu
+                if (Utility.pritisnuto == 0) {
+                    Utility.kreairajNarudžbenicu(customerID);
+                    Utility.pritisnuto = 1;
                 }
-                else {
-                    String query = "INSERT INTO narudzbenica(kupac_id,datum_narudzbe, state) VALUES('" + customerID + "','" + DateTime.Now.ToString("yyyy-MM-dd") + "', 0)";
-                    Utility.executeQuery(query, 0);
-                    Utility.stopQuery(0);
-                    state = 0;
+                //Uzmi novokreiranu narudzbenicu
+                TakeLastOrder();
+
+                //Provjeri da li postoji tog proizvoda toliko
+                ProvjeriStanje();
+                if (kolicinaStanjeKolicina < 0) {
+                    MessageBox.Show("Trenutno nemamo toliko proizvoda na stanju.");
+                    return;
                 }
+
+                //Ažuriraj bazu sa podacima
+                String queryOne = "INSERT INTO stavka_narudzbenice(narudzbenica_id,artikal_id,kolicina) VALUES('" + lastOrder + "','" + textBoxID.Text + "','" + textBoxKOL.Text + "')";
+                Utility.executeQuery(queryOne, 0);
+
+                //Prikaži u korpi
+                Cart();
                 Utility.stopQuery(2);
+
+                //Ažuriraj skladište
+                String queryThree = "UPDATE skladiste SET kolicina_stanje='" + kolicinaStanjeKolicina + "' WHERE artikal_id='" + textBoxID.Text + "'";
+                Utility.executeQuery(queryThree, 0);
+                Utility.stopQuery(0);
             }
             catch (Exception ex) {
                 MessageBox.Show(ex.ToString());
             }
 
-            if (state == 1) {
-                Utility.pritisnuto = 0;
-            }
-            if (state == 0) {
-                Utility.pritisnuto = 1;
-            }
-
-            if (Utility.pritisnuto == 0) {
-                String query = "INSERT INTO narudzbenica(kupac_id,datum_narudzbe, state) VALUES('" + customerID + "','" + DateTime.Now.ToString("yyyy-MM-dd") + "', 0)";
-                Utility.executeQuery(query, 0);
-                Utility.stopQuery(0);
-                Utility.pritisnuto = 1;
-            }
-            
-            TakeLastOrder();
-
-            //String queryOne = "INSERT INTO stavka_narudzbenice(narudzbenica_id,artikal_id,kolicina) VALUES('" + lastOrder + "','" + textBoxID.Text + "','" + textBoxKOL.Text + "')";
-            //Utility.executeQuery(queryOne, 0);
-            //Cart();
-
-            String queryTwo = "SELECT kolicina_stanje FROM skladiste WHERE artikal_id='" + textBoxID.Text + "'";
-            Utility.executeQuery(queryTwo, 2);
-            reader = Utility.reader;
-            reader.Read();
-
-            int kolicinaStanjeKolicina = Convert.ToInt32(reader[0]) - Convert.ToInt32(textBoxKOL.Text);
-            if (kolicinaStanjeKolicina < 0) {
-                MessageBox.Show("Trenutno nemamo toliko proizvoda na stanju.");
-                return;
-            }
-            String queryOne = "INSERT INTO stavka_narudzbenice(narudzbenica_id,artikal_id,kolicina) VALUES('" + lastOrder + "','" + textBoxID.Text + "','" + textBoxKOL.Text + "')";
-            Utility.executeQuery(queryOne, 0);
-            Cart();
-            Utility.stopQuery(2);
-            String queryThree = "UPDATE skladiste SET kolicina_stanje='" + kolicinaStanjeKolicina + "' WHERE artikal_id='" + textBoxID.Text + "'";
-            Utility.executeQuery(queryThree, 0);
-            Utility.stopQuery(0);
-          
+            //Ažuriraj sve ostalo
             ShowArticles();
             Cart();
             TotalPrice();
@@ -233,25 +252,36 @@ namespace Projektni_zadatak
             textBoxTotal.Text = totalPrice.ToString();
         }
 
+        private void CheckKorpu() {
+            String queryHelper = "SELECT sn.artikal_id FROM narudzbenica n, stavka_narudzbenice sn WHERE n.narudzbenica_id = sn.narudzbenica_id AND n.kupac_id = '" + customerID + "'";
+            Utility.executeQuery(queryHelper, 2);
+            reader = Utility.reader;
+            while (reader.Read())
+            {
+                if (reader[0].ToString() == textBoxID.Text) artikalValidan = 1;
+                else artikalValidan = 0;
+            }
+        }
+
         private void buttonDelete_Click(object sender, EventArgs e)
         {
+            errorProvider1.Clear();
             try
             {
-
+                
+                //Provjeri je li uopste unesen id
                 if (CheckTextBoxId() == 1) return;
 
-                String queryHelper = "SELECT sn.artikal_id FROM narudzbenica n, stavka_narudzbenice sn WHERE n.narudzbenica_id = sn.narudzbenica_id AND n.kupac_id = '"+customerID+"'";
-                Utility.executeQuery(queryHelper, 2);
-                reader = Utility.reader;
-                while (reader.Read()) {
-                    if (reader[0].ToString() == textBoxID.Text) dobar = 1;
-                }
-                if (dobar == 0) {
-                    MessageBox.Show("Molimo vas unesite pravilan id artikla iz stavke narudzbenice.");
+                //Provjeri je li element koji se briše validan
+                CheckKorpu();
+                
+                //Ukoliko nije, izbaci grešku
+                if (artikalValidan == 0)
+                {
+                    MessageBox.Show("Molimo vas unesite pravilan id artikla iz korpe.");
+                    errorProvider1.SetError(textBoxID, "Pogrešan id.");
                     return;
                 }
-                //if (reader.HasRows) MessageBox.Show("IMA");
-                //else MessageBox.Show("NEMA");
                 Utility.stopQuery(2);
 
 
@@ -268,11 +298,8 @@ namespace Projektni_zadatak
                 Utility.executeQuery(queryTwo, 2);
                 reader = Utility.reader;
                 reader.Read();
-
-
                 String amount = reader[0].ToString();
                 String amountHelper = reader[1].ToString();
-
 
                 int final = Convert.ToInt32(amount) + Convert.ToInt32(amountHelper);
 
@@ -293,7 +320,13 @@ namespace Projektni_zadatak
                 TotalPrice();
             }
             catch (Exception ex) {
-                MessageBox.Show(ex.ToString());
+                if (ex.ToString().Contains("0x80004005"))
+                {
+                    MessageBox.Show("Korpa je prazna!");
+                }
+                else {
+                    MessageBox.Show(ex.ToString());
+                }
             }
 
         }
